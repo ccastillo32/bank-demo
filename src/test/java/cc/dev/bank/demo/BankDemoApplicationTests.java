87ea2f971b2617cc35b2695e2d4f6936a4ce8431
ccastillo32 <cristiancastillo32@gmail.com>
2024-02-07 18:19:54 -0500

    Initial configuration

---
+++ src/test/java/cc/dev/bank/demo/BankDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.dev.bank.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
